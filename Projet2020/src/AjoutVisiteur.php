656f895739f585df4a43f79764d040ec1003d4d6
Novel Master <62702890+Enigmaster-bis@users.noreply.github.com>
2020-09-15 09:54:02 +0200

    Update AjoutVisiteur.php

--- Projet2020/src/AjoutVisiteur.php
+++ Projet2020/src/AjoutVisiteur.php
@@ -41,11 +41,6 @@ include '../include/top.php';
 	    	<label for="pwd">Mot de passe : </label>
 			<input id="pwd" name="pwd" type="password" value="" size="30" maxlength="30"/>
 	    <br>
-	    <p></p>
-	    	<label for="login">Login : </label>
-			<input id="login" name="login" type="text" value="" size="6" maxlength="6"/>
-	    <br>
-	    <p></p>
 	    <label for="dteEmbauche">Date d'embauche : </label>
 			<input id="dteEmbauche" name="dteEmbauche" type="date" value=""/>
 	    <br>
